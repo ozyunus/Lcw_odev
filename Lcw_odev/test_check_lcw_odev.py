629a7db245865536c030dee2ed0cce9a5e60da3b
Yunus Oz <ozyunus@msn.com>
2022-09-07 21:16:42 +0300

    Add files via upload

--- Lcw_odev/test_check_lcw_odev.py
+++ Lcw_odev/test_check_lcw_odev.py
@@ -1,56 +1,36 @@
 import unittest
-from selenium.webdriver import ActionChains
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as ec
 
 
 class TestCheckLcwOdev(unittest.TestCase):
-    CATEGORY_PAGE = (By.CSS_SELECTOR, ".menu-header-item  ")
-    CATEGORY_PAGE_2 = (By.CLASS_NAME, 'visual-item__anchor__text')
-    PRODUCT_PAGE = (By.CLASS_NAME, "product-card--one-of-4")
-    CHOICE_SIZE = (By.XPATH, "//div[@id='option-size']//a[not(contains(@data-stock, '0'))]")
-    ADD_TO_CART = (By.ID, "pd_add_to_cart")
-    CART_PAGE = (By.CLASS_NAME, "dropdown-label")
-    MAIN_PAGE = (By.CLASS_NAME, "main-header-logo")
-    PAYMENT_STEP = (By.CLASS_NAME, 'pr-20')
 
-    base_url = 'https://www.lcwaikiki.com/tr-TR/TR'
+    CATEGORY_PAGE = (By.CSS_SELECTOR, ".menu-header-item  ")  # Erkek Kategorisini Seç [1]
+    CATEGORY_PAGE_2 = (By.CLASS_NAME, "visible-md")  # Alt kategori [0]
+    PRODUCT_PAGE = (By.CLASS_NAME, "product-card.product-card--one-of-4")  # Ürün Seç [0]
+    CHOICE_SIZE = (By.XPATH, '//*[@id="option-size"]/a')  # Beden Seç [1]
+    ADD_TO_CART = (By.ID, "pd_add_to_cart")
+    CART_PAGE = (By.CLASS_NAME, "dropdown-label")  # Sepet'e git [2]
+    MAIN_PAGE = (By.CLASS_NAME, "main-header-logo")
+
 
     def setUp(self):
-        option = Options()
-        option.add_argument('--disable-extensions')
-        self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=option)
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
         self.driver.maximize_window()
+        self.driver.get('https://www.lcwaikiki.com/tr-TR/TR')
         self.driver.implicitly_wait(10)
         self.wait = WebDriverWait(self.driver, 10)
 
     def test_check_lcw_odev(self):
-        self.driver.get(self.base_url)
-        self.assertIn('LC Waikiki', self.driver.title)
+        self.wait.until(ec.presence_of_all_elements_located(self.CATEGORY_PAGE))[1].click()
+        self.wait.until(ec.presence_of_all_elements_located(self.CATEGORY_PAGE_2))[0].click()
+        self.wait.until(ec.presence_of_all_elements_located(self.PRODUCT_PAGE))[3].click()
+        self.wait.until(ec.presence_of_all_elements_located(self.CHOICE_SIZE))[2].click()
+        self.wait.until(ec.element_to_be_clickable(self.ADD_TO_CART)).click()
+        self.wait.until(ec.presence_of_all_elements_located(self.CART_PAGE))[2].click()
+        self.wait.until(ec.element_to_be_clickable(self.MAIN_PAGE)).click()
 
-        element = self.driver.find_element(*self.CATEGORY_PAGE)
-        ActionChains(self.driver).move_to_element(element).perform()
-
-        self.driver.find_element(*self.CATEGORY_PAGE_2).click()
-        self.assertIn('Kadın', self.driver.title)
-
-        self.driver.find_elements(*self.PRODUCT_PAGE)[4].click()
-        self.assertTrue(self.wait.until(
-            EC.element_to_be_clickable(self.ADD_TO_CART)).text)
-
-        self.driver.find_element(*self.CHOICE_SIZE).click()
-        self.driver.find_element(*self.ADD_TO_CART).click()
-
-        self.driver.find_elements(*self.CART_PAGE)[2].click()
-        self.assertEqual('ÖDEME ADIMINA GEÇ', self.wait.until(
-            EC.element_to_be_clickable(self.PAYMENT_STEP)).text)
-
-        self.driver.find_element(*self.MAIN_PAGE).click()
-        self.assertIn('LC Waikiki', self.driver.title)
-
-    def tearDown(self):
-        self.driver.close()
+        self.driver.quit()
